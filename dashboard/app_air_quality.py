2a42daa3eb91075e66ea9c9591df3f04fd393f24
Pra5etya <careermagic780@gmail.com>
2024-07-09 16:06:48 +0700

    mod

--- dashboard/app_air_quality.py
+++ dashboard/app_air_quality.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63cc9beef62f9e56c2ad7134baee0b9757aa3804f3d4c67ac8ada8df66f74087
-size 8837
+oid sha256:b47046430346a91768b4065546d4e58c4657d4fbffe8c6f75714e6214608d1ce
+size 14352
